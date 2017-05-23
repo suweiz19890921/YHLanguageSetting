a7a2f45526e7e92d9b84fe4dd38a0f463d83b6a1
liao <liao@solot.com>
2017-05-23 09:14:05 +0000

    Update LanguageSettingViewController.m

--- YHLanguageSetting/LanguageSettingViewController.m
+++ YHLanguageSetting/LanguageSettingViewController.m
@@ -79,7 +79,7 @@ static NSString * const LanguageCellIdentifier = @"LanguageCellIdentifier";
 //    self.navigationItem.leftBarButtonItem = leftItem;
     
     
-    UIBarButtonItem *rightItem = [[UIBarButtonItem alloc] initWithTitle:Locale(@"保存") style:0 target:self action:@selector(saveAction:)];
+    UIBarButtonItem *rightItem = [[UIBarButtonItem alloc] initWithTitle:Locale(@"New_CreateSpot_Save") style:0 target:self action:@selector(saveAction:)];
     self.navigationItem.rightBarButtonItem = rightItem;
 }
 
